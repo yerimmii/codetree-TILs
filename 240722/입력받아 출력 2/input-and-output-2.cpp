260fc1fc013661ba304229b06b22405163328cd9
kimyr04 <126783800+yerimmii@users.noreply.github.com>
2024-07-22 22:01:03 +0900

    Title: 입력받아 출력 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240722/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int front, back;
+    
+    cin >> front;
+    cin.get();
+    cin >> back;
+
+    cout << front << back;
+
+    return 0;
+}
