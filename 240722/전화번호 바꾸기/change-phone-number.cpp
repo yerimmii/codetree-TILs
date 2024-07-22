a3d6966101dd035e58d2c1795861567ed2f56752
kimyr04 <126783800+yerimmii@users.noreply.github.com>
2024-07-22 22:08:18 +0900

    Title: 전화번호 바꾸기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240722/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int start, mid, last;
+    char c;
+    cin >> start >> c >> mid >> c >> last;
+
+    cout << "010-" << last << "-" << mid; 
+
+    return 0;
+}
