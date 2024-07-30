6d3cbb412ccb5c27a757afc2e750eb046c8ccb7e
kimyr04 <126783800+yerimmii@users.noreply.github.com>
2024-07-30 15:50:58 +0900

    Title: 시험 통과 여부 확인하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240730/시험 통과 여부 확인하기/verify-test-passed.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    if (n >= 80) {
+        cout << "pass";
+    } else {
+        cout << 80 - n << " more score";
+    }
+    
+    return 0;
+}
