36cad9b84f3678b9a6bf4f8bc3d125ead3ce0595
kimyr04 <126783800+yerimmii@users.noreply.github.com>
2024-07-31 16:25:28 +0900

    Title: 특정 조건 세 정수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240731/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
+++ 240731/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
@@ -7,14 +7,14 @@ int main() {
 
     cin >> a >> b >> c;
 
-    if (a > b) {
-        if (a > c) {
+    if (a < b) {
+        if (a < c) {
             min = a;
         } else {
             min = c;
         }
     } else {
-        if (b > c) {
+        if (b < c) {
             min = b;
         } else {
             min = c;
